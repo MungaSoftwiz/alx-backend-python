e89e163da1b559ca43ab671d51391d46724cd426
MungaSoftwiz <mwachilumobm@gmail.com>
2024-07-05 01:35:55 +0300

    Define and annotate variables

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Describes annotated variables """
+from typing import TypeVar
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
